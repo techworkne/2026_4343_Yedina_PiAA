3fc96cb8d7e4bdbbc57557585a01e78c40251d8b
Анастасия Едина <techwork.ne@yandex.kz>
2026-05-05 15:00:55 +0300

    forgotten logs

--- lab4/src/stepik1.cpp
+++ lab4/src/stepik1.cpp
@@ -5,18 +5,27 @@
 using namespace std;
 
 vector<int> prefic_func(string str){
+    cout << "\nвычисление префикс-функции для строки: " << str << "\n";
     vector<int> p(str.size(), 0);
-    p.push_back(0);
     for (int i = 1; i < str.size(); i++){
         int k = p[i - 1];
+        cout << "\ni = " << i << ", символ '" << str[i] << "', начальное k = " << k << endl;
         while (k > 0 && str[i] != str[k]){
+            cout << "    несовпадение '" << str[i] << "' != '" << str[k] << "', k = p[" << k-1 << "] = " << p[k-1] << endl;
             k = p[k - 1];
         }
         if (str[i] == str[k]){
+            cout << "    совпадение '" << str[i] << "' == '" << str[k] << "', k++ => " << k+1 << endl;
             k++;
+        } else {
+            cout << "    нет совпадения, k остаётся " << k << endl;
         }
         p[i] = k;
+        cout << "  p[" << i << "] = " << k << endl;
     }
+    cout << "префикс-функция: ";
+    for (int v : p) cout << v << " ";
+    cout << endl;
     return p;
 }
 
@@ -28,13 +37,22 @@ vector<int> kmp(string p, string t){
 
     string combined = p + "#" + t;
 
+    cout << "\nформирование комбинированной строки: " << combined << "\n";
+    cout << "длина шаблона = " << lengthP << endl;
+
     vector<int> pi = prefic_func(combined);
     int count = 0;
 
+    cout << "\nпоиск вхождений (позиции pi[i] == " << lengthP << ")\n";
+
     for (int i = lengthP; i < combined.size(); i++){
+        cout << "i = " << i << ", pi[" << i << "] = " << pi[i] << " (символ '" << combined[i] << "')";
         if (pi[i] == lengthP){
             int pos = i - 2 * lengthP;
+            cout << " найдено вхождение. позиция в тексте = " << pos << endl;
             ans.push_back(pos);
+        } else {
+            cout << endl;
         }
     }
     return ans;
--- lab4/src/stepik2.cpp
+++ lab4/src/stepik2.cpp
@@ -5,50 +5,73 @@
 using namespace std;
 
 vector<int> prefic_func(string str){
+    cout << "\nпрефикс-функция для строки B = \"" << str << "\"\n";
     vector<int> p(str.size(), 0);
     for (int i = 1; i < str.size(); i++){
         int k = p[i - 1];
+        cout << "  i=" << i << " char='" << str[i] << "' k=" << k;
         while (k > 0 && str[i] != str[k]){
+            cout << "    несовпадение '" << str[i] << "' != '" << str[k] << "', k = p[" << k-1 << "] = " << p[k-1] << endl;
             k = p[k - 1];
         }
         if (str[i] == str[k]){
             k++;
+            cout << " -> совпадение, k=" << k;
+        } else {
+            cout << " -> нет совпадения, k=" << k;
         }
         p[i] = k;
+        cout << " -> p[" << i << "]=" << k << endl;
     }
+    cout << "итоговая префикс-функция: ";
+    for (int v : p) cout << v << " ";
+    cout << endl;
     return p;
 }
 
 
 int kmp_cycle(string A, string B){
-    int lengthB = B.size();
     vector<int> ans;
 
     vector<int> pi = prefic_func(B);
     int k = 0; // нынешнее состояния совпадения
     int n = A.size();
 
+    cout << "A = " << A << ", B = " << B << ", длина n = " << n << endl;
+
+    cout << "\nпроход по удвоенной строке A\n";
     for (int i = 0; i < 2*n ; i++)
     {
         // с помощью модульнрой арифметики избегаю создания 2А. abcabc
         // a -> i = 0 -> 0%3 = 0; 1%3=1; 2%3=2; 3%3=0; 4%3=1 ...
         char current_char = A[i % n];
+        cout << "\nшаг i=" << i << " (символ A[" << i % n << "] = '" << current_char
+             << "'), текущее совпадение k=" << k;
 
         while (k > 0 && current_char != B[k]){
+            cout << " -> несовпадение с B[" << k << "]='" << B[k] << "', переходим k=pi[" << k-1 << "]=" << pi[k-1];
             k = pi[k - 1];
         }
         if (current_char == B[k]){
             k++;
+            cout << " -> символы совпали (B[" << k-1 << "]='" << B[k-1] << "'), k++ => " << k;
+        } else {
+            cout << " -> нет совпадения, k остаётся " << k;
         }
 
         if (k == n){
             int pos = i - n + 1;
-            if (pos < n) return pos;
-            else
+            cout << "позиция начала вхождения в строке: pos = " << i << " - " << n << " + 1 = " << pos << endl;
+            if (pos < n){
+                cout << "pos < n => возвращаем " << pos << " (циклический сдвиг на " << pos << " влево)\n";
+                return pos;
+            }else{
+                cout << "pos >= n, но такого не должно случиться, возвращаем -1\n";
                 return -1;
+            }
         }
     }
-
+    cout << "\nциклический сдвиг не найден за 2n итераций\n";
     return -1;
 }
 
@@ -57,6 +80,7 @@ int main(){
     cin >> A >> B;
 
     if (A.size() != B.size()){
+        cout << "строки разной длины => -1\n";
         cout << -1 << endl;
         return 0;
     }
